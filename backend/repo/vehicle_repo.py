56f626ba61b906744bf5d1dfea0c2e6174a3c0fd
Ashutosh Mishra <ashu7mishra@gmail.com>
2025-07-04 07:36:48 +0530

    [skip ci] adding vehicles details

--- backend/repo/vehicle_repo.py
+++ backend/repo/vehicle_repo.py
@@ -6,3 +6,7 @@ class VehicleRepo:
         if vehicle_number in self.vehicle_map:
             return self.vehicle_map[vehicle_number]
         return None
+    
+    def save_vehicle(self, vehicle):
+        self.vehicle_map[vehicle.number] = vehicle
+        return vehicle
--- backend/service/ticket_service.py
+++ backend/service/ticket_service.py
@@ -1,10 +1,12 @@
 from models.ticket import Ticket
 from datetime import datetime
 from repo.gate_repo import GateRepo
+from repo.vehicle_repo import VehicleRepo
 
 class TicketService:
-    def __init__(self, GateRepo):
+    def __init__(self, GateRepo, VehicleRepo):
         self.GateRepo = GateRepo
+        self.VehicleRepo = VehicleRepo
 
     def issueTicket(self, vehicle_number, owner_name, gate_id, vehicle_type) -> Ticket:
 
@@ -13,11 +15,16 @@ class TicketService:
                         parking_slot=None, generated_gate=None)
         # set info like gate no
         gate = self.GateRepo.find_gate_by_id(gate_id)
-        
+
         if gate is None:
             raise Exception('Gate not found')
         
         ticket.generated_gate = gate
+
+        #vehicle info
+        vehicle = self.VehicleRepo.find_vehicle_by_number(vehicle_number)
+        if vehicle is None:
+            raise Exception('vehicle not found ')
         # find a slot
         # update slot
         # update parking counters
